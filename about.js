7cdfc5340614256bf82fa6f802c0310d64e496ee
codedbyyashwanth <61375676+codedbyyashwanth@users.noreply.github.com>
2021-02-24 18:27:50 +0530

    Updated About Page

--- about.js
+++ about.js
@@ -12,3 +12,50 @@ function menuBtnClicked() {
     navBar.classList.add("nav-open");
   }
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex1 = 0;
+showSlidesAuto();
+
+function showSlidesAuto() {
+  var i;
+  var slide = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slideIndex1++;
+  if (slideIndex1 > slide.length) {
+    slideIndex1 = 1;
+  }
+  slide[slideIndex1 - 1].style.display = "block";
+  setTimeout(showSlidesAuto, 4000);
+}
